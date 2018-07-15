04330cf5930d3fb08dcff79b7c14194372abdbf0
Shinya Yamada <syamada@student.42.us.org>
2018-07-15 16:01:45 -0700

    add srcs

---
+++ srcs/validate_input.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validate_input.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: syamada <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/15 15:24:52 by syamada           #+#    #+#             */
+/*   Updated: 2018/07/15 16:00:57 by syamada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "fillit.h"
+#include <stdio.h>
+
+static int	is_validsquare(char **str)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (str[i])
+		printf("str: %s\n", str[i++]);
+	return (0);
+}
+
+int		validate_input(char ***str)
+{
+	int		i;
+
+	i = 0;
+	/*
+	while (str[i])
+		is_validblock((char**)str[i++]);
+		*/
+	return (0);
+}
