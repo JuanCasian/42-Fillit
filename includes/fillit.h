db4863c43b4d68c4245c5ac22bff9859d299060c
Juan Pedro Casian <juanpedrocasian@gmail.com>
2018-07-15 17:30:55 -0700

    Adding new structures to header file

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: jcasian <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/14 18:34:58 by jcasian           #+#    #+#             */
-/*   Updated: 2018/07/14 21:05:30 by syamada          ###   ########.fr       */
+/*   Updated: 2018/07/15 17:27:33 by jcasian          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,18 @@
 
 # define	BUFSIZE 4096
 # include <fcntl.h>
+
+typedef struct	position
+{
+	int 		x;
+	int 		y;
+}				pos;
+
+typedef struct	tetrispieces
+{
+	pos			pieces[4];
+	char		name;
+}				tetrimonio;
 
 int		validate_input(char *str);
 void	fillit(int fd);
